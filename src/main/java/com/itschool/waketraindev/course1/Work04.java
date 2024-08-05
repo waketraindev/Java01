45b7658e9088c09d8e3fc41b9a148abb2c7f2920
WakeTrainDev <175499942+waketraindev@users.noreply.github.com>
2024-08-05 20:09:44 +0300

    Autoclose the scanner

--- src/main/java/com/itschool/waketraindev/course1/Work04.java
+++ src/main/java/com/itschool/waketraindev/course1/Work04.java
@@ -10,17 +10,17 @@ import java.util.Scanner;
 
 public class Work04 {
     public static void main(String[] args) {
-        Scanner inputScanner = new Scanner(System.in);
-        System.out.print("Age (years): ");
-        final int age = inputScanner.nextInt();
-        System.out.print("Height (centimeters): ");
-        final int height = inputScanner.nextInt();
-        System.out.print("Weight (kilograms): ");
-        final double weight = inputScanner.nextDouble();
+        try (Scanner inputScanner = new Scanner(System.in)) {
+            System.out.print("Age (years): ");
+            final int age = inputScanner.nextInt();
+            System.out.print("Height (centimeters): ");
+            final int height = inputScanner.nextInt();
+            System.out.print("Weight (kilograms): ");
+            final double weight = inputScanner.nextDouble();
 
-        final int maxHeartRate = 220 - age;
-        final double bmi = (weight / height / height * 10_000);
-        System.out.printf("BMI: %.2f, Max Heart Rate: %d %n", bmi, maxHeartRate);
-
+            final int maxHeartRate = 220 - age;
+            final double bmi = (weight / height / height * 10_000);
+            System.out.printf("BMI: %.2f, Max Heart Rate: %d %n", bmi, maxHeartRate);
+        }
     }
 }
