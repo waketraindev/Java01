109f484f5a60a0496f753ed3759238d12f487b61
WakeTrainDev <175499942+waketraindev@users.noreply.github.com>
2024-08-05 20:10:00 +0300

    Run all the examples

--- src/main/java/com/itschool/waketraindev/Main.java
+++ src/main/java/com/itschool/waketraindev/Main.java
@@ -1,6 +1,15 @@
 package com.itschool.waketraindev;
 
+import com.itschool.waketraindev.course1.Work01;
+import com.itschool.waketraindev.course1.Work02;
+import com.itschool.waketraindev.course1.Work03;
+import com.itschool.waketraindev.course1.Work04;
+
 public class Main {
     public static void main(String[] args) {
+        Work01.main(args);
+        Work02.main(args);
+        Work03.main(args);
+        Work04.main(args);
     }
 }
