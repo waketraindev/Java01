215c536e361af986d3c51a8fa32e24de9731f7d5
WakeTrainDev <175499942+waketraindev@users.noreply.github.com>
2024-08-05 19:21:19 +0300

    Add prior work

---
+++ src/main/java/com/itschool/waketraindev/course1/Work01.java
@@ -0,0 +1,18 @@
+package com.itschool.waketraindev.course1;
+
+/*
+ * Description:
+ * 1. Simple Output Program: Write a program that prints your name, age, and favorite color on three separate lines.
+ */
+
+public class Work01 {
+    public static void main(String[] args) {
+
+        final String firstName = "Eleni";
+        final String lastName = "Haynes";
+        final Integer age = 36;
+        final String color = "black";
+
+        System.out.printf("Name: %s %s%nAge: %d%nColor: %s%n", firstName, lastName, age, color);
+    }
+}
